86dd4d3f4477ffc2e097ae7e3909cfb1a8ae5db2
unknown <csaa2019@mymail.pomona.edu>
2022-02-10 21:23:57 -0800

    added bitblt-- see comments

--- engine/src/image.rs
+++ engine/src/image.rs
@@ -1,3 +1,6 @@
+use image::Triangle;
+use itertools::Itertools;
+
 type Color = (u8, u8, u8, u8);
 
 // const WIDTH: usize = 320;
@@ -15,6 +18,9 @@ impl Rect {
     pub fn new(x0: i32, y0: i32, w: u32, h: u32) -> Rect {
         Rect { x0, y0, w, h }
     }
+    // TODO: Add rect_inside function which will check if a point is inside the rect.
+
+    // This is some changing height and width code in case we want it.
 
     // pub fn change_h(&mut self, inc: i32) {
     //     let h = (self.h as i32) + inc;
@@ -29,11 +35,11 @@ impl Rect {
     //         self.w = w as u32;
     //     }
     // }
-
-    // Maybe add functions to test if a point is inside the rect...
-    // Or whether two rects overlap...
 }
-
+/**
+ * This is basically a struct that contains a framebuffer.
+ * It also contains a width and a height.
+ */
 pub struct Image {
     pub buffer: Box<[Color]>, // or Vec<Color>, or...
     pub w: usize,
@@ -49,6 +55,31 @@ impl Image {
         }
     }
 
+    /**
+     * This function will create an Image if you give it a filename, a width, and a height.
+     * This can be used to load in textures, spritesheets, etc.
+     */
+    pub fn from_png(filename: &str, img_width: u32, img_height: u32) -> Image {
+        let (image_data, dimensions) = {
+            let img = image::open(filename).unwrap();
+            let img_resized = image::imageops::resize(&img, img_width, img_height, Triangle);
+            let dim = img_resized.dimensions();
+            (img_resized.into_raw(), dim)
+        };
+        let mut texture = Vec::<Color>::new();
+
+        for (a, b, c, d) in image_data.into_iter().tuples() {
+            let c: Color = (a, b, c, d);
+            texture.push(c);
+        }
+
+        Image {
+            buffer: texture.into_boxed_slice(),
+            w: img_width as usize,
+            h: img_height as usize,
+        }
+    }
+
     fn clear(&mut self, c: Color) {
         self.buffer.fill(c);
     }
@@ -57,12 +88,98 @@ impl Image {
         &self.buffer
     }
 
-    pub fn bitblt(&self) {
-        // TODO: write bitblt function
+    /**
+     * This is the bitblt function.
+     * It is a method called on an Image struct.
+     * Ideally, you call this on your framebuffer.
+     * You pass it a src_img, which is ALSO an image struct. This is the texture.
+     * The function above, from_png, will make you an Image from a file you pass to it
+     * and the dimensions you specify.
+     *
+     * The from &rect is the part of the source image you want to copy over.
+     * The to (i32, i32) is the coordinates of where to paste this over to in the framebuffer.
+     *
+     * For example, in my code, this is how I called bitblt:
+     *       fb2d.bitblt(&texture, &new_rect, (10, 10));
+     *
+     * NOTE: The size of your texture (wxh) should be the SAME as your rect's wxh.
+     * This can be achieved by passing the rectangle's wxh into from_png
+     * when creating your texture Image. If they are not the same, the texture will get cut off.
+     *
+     * (Nate or Grace, maybe you could figure out a way to just make it resize?)
+     */
+    fn bitblt(&mut self, src_img: &Image, from: &Rect, to: (i32, i32)) {
+        let dst = &mut self.buffer;
+        let dst_size = (self.w, self.h);
+        let src = src_img.as_slice();
+        let src_size = (src_img.w, src_img.h);
+
+        // implement after writing a rect_inside
+        // assert!(rect_inside(from, (0, 0, src_size.0, src_size.1)));
+        let (to_x, to_y) = to;
+        if (to_x + from.w as i32) < 0
+            || (dst_size.0 as i32) <= to_x
+            || (to_y + from.h as i32) < 0
+            || (dst_size.1 as i32) <= to_y
+        {
+            return;
+        }
+        let src_pitch = src_size.0;
+        let dst_pitch = dst_size.0;
+        // All this rigmarole is just to avoid bounds checks on each pixel of the blit.
+        // We want to calculate which row/col of the src image to start at and which to end at.
+        // This way there's no need to even check for out of bounds draws---
+        // we'll skip rows that are off the top or off the bottom of the image
+        // and skip columns off the left or right sides.
+        let y_skip = to_y.max(0) - to_y;
+        let x_skip = to_x.max(0) - to_x;
+        let y_count = (to_y + from.h as i32).min(dst_size.1 as i32) - to_y;
+        let x_count = (to_x + from.w as i32).min(dst_size.0 as i32) - to_x;
+        // The code above is gnarly so these are just for safety:
+        debug_assert!(0 <= x_skip);
+        debug_assert!(0 <= y_skip);
+        debug_assert!(0 <= x_count);
+        debug_assert!(0 <= y_count);
+        debug_assert!(x_count <= from.w as i32);
+        debug_assert!(y_count <= from.h as i32);
+        debug_assert!(0 <= to_x + x_skip);
+        debug_assert!(0 <= to_y + y_skip);
+        debug_assert!(0 <= from.x0 as i32 + x_skip);
+        debug_assert!(0 <= from.y0 as i32 + y_skip);
+        debug_assert!(to_x + x_count <= dst_size.0 as i32);
+        debug_assert!(to_y + y_count <= dst_size.1 as i32);
+        // OK, let's do some copying now
+        for (row_a, row_b) in src
+        // From the first pixel of the top row to the first pixel of the row past the bottom...
+            [(src_pitch * (from.y0 as i32 + y_skip) as usize)..(src_pitch * (from.y0 as i32 + y_count) as usize)]
+        // For each whole row...
+            .chunks_exact(src_pitch)
+        // Tie it up with the corresponding row from dst
+            .zip(
+                dst[(dst_pitch * (to_y + y_skip) as usize)
+                    ..(dst_pitch * (to_y + y_count) as usize)]
+                    .chunks_exact_mut(dst_pitch),
+            )
+        {
+            // Get column iterators, save on indexing overhead
+            let to_cols = row_b
+                [((to_x + x_skip) as usize)..((to_x + x_count) as usize)].iter_mut();
+            let from_cols = row_a
+                [((from.x0 as i32 + x_skip) as usize)..((from.x0 as i32 + x_count) as usize)].iter();
+            // Composite over, assume premultiplied rgba8888 in src!
+            for (to, from) in to_cols.zip(from_cols) {
+                let ta = to.3 as f32 / 255.0;
+                let fa = from.3 as f32 / 255.0;
+                to.0 = from.0.saturating_add((to.0 as f32 * (1.0 - fa)).round() as u8);
+                to.1 = from.1.saturating_add((to.1 as f32 * (1.0 - fa)).round() as u8);
+                to.2 = from.2.saturating_add((to.2 as f32 * (1.0 - fa)).round() as u8);
+                to.3 = ((fa + ta * (1.0 - fa)) * 255.0).round() as u8;
+            }
+        }
     }
 
     // THE FOLLOWING CODE IS MY STUFF FROM THE RECTANGLE LAB
-    // MIGHT BE USEFUL FOR THE GAME
+    // MIGHT BE USEFUL FOR THE GAME??
 
     // #[allow(dead_code)]
     // fn hline(&mut self, x0: usize, x1: usize, y: usize, c: Color) {
